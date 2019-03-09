5d86f1178f617d438e050613e905a9740f9cb7b6
kopylovvlad <kopylov.vlad@gmail.com>
2019-03-09 22:07:08 +0300

    fix

--- lib/hamdown_core/engine.rb
+++ lib/hamdown_core/engine.rb
@@ -8,7 +8,7 @@ module HamdownCore
     def self.call(content)
       ast = HamdownCore::Parser.new.call(content)
       transformed_ast = HamdownCore::Transformer.call(ast)
-      puts HamdownCore::Compiler.call(transformed_ast)
+      HamdownCore::Compiler.call(transformed_ast)
     end
   end
 end
